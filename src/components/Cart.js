fd62af3060312afdc999a464de44bf1ba877adb2
Sahil Gupta <sahilgupta742@gmail.com>
2024-01-04 21:10:41 +0530

    Adding Item Total using Redux

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,7 +5,7 @@ import { IoTrashBin } from "react-icons/io5";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
-  // console.log(cartItems);
+  const itemTotal = useSelector((store) => store.cart.totalPrice);
 
   const dispatch = useDispatch();
 
@@ -16,11 +16,11 @@ const Cart = () => {
 
   const handleRemoveItem = (item) => {
     //dispatching an action
-    dispatch(removeItems(item.card.info.id));
+    dispatch(removeItems(item));
   };
 
   return (
-    <div className="text-center m-4 p-4">
+    <div className="text-center m-4 p-4 bg-mid-pink">
       <h1 className="text-2xl font-bold">Cart</h1>
 
       <button
@@ -34,15 +34,12 @@ const Cart = () => {
         <h1 className="text-center m-5 text-3xl">Cart is Empty</h1>
       )}
 
-      <div className="w-6/12 m-auto">
+      <div className="w-6/12 bg-white text-sm font-normal">
         {cartItems.map((item) => (
-          <div
-            key={item.card.info.id}
-            className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between "
-          >
-            <div className="w-3/12">
+          <div key={item.card.info.id} className="p-2 m-2  text-left flex  ">
+            <div className="w-2/12 mr-2">
               <img
-                className="w-28"
+                className="w-20"
                 src={CDN_URL + item.card.info.imageId}
                 alt="dishImg"
               ></img>
@@ -50,21 +47,26 @@ const Cart = () => {
             <div className="w-9/12 flex items-center justify-between ">
               <div className=" py-2">
                 <span>{item?.card?.info?.name}</span>
-                <br></br>
-                <span>
+              </div>
+              <div className="flex items-center">
+                <div className="mr-8">
                   ₹
                   {item?.card?.info?.price
                     ? item?.card?.info?.price / 100
                     : item?.card?.info?.defaultPrice / 100}
-                </span>
+                </div>
+                <IoTrashBin
+                  className="text-xl w-6 h-6 m-1 cursor-pointer hover:scale-110"
+                  onClick={() => handleRemoveItem(item)}
+                />
               </div>
-              <IoTrashBin
-                className="text-xl w-6 h-6 m-1 cursor-pointer hover:scale-110"
-                onClick={() => handleRemoveItem(item)}
-              />
             </div>
           </div>
         ))}
+        <div className="text-left">
+          <h1>Bill Details</h1>
+          <h2>Item Total ₹{itemTotal / 100} </h2>
+        </div>
       </div>
 
       <div></div>
--- src/utils/redux/cartSlice.js
+++ src/utils/redux/cartSlice.js
@@ -4,23 +4,33 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
+    totalPrice: 0,
   },
   reducers: {
     addItems: (state, action) => {
       //Mutating the state here
       state.items.push(action.payload);
+      const newItemPrice =
+        action.payload.card.info.price || action.payload.card.info.defaultPrice; // Get item price
+      state.totalPrice += newItemPrice;
     },
     removeItems: (state, action) => {
       //Get the id of item to remove from action
-      // Filter items array to remove that item
+      // Filter items array to remove that item, only return the items which doesnot match with id
       // Return updated items array in state
-      const itemIdToRemove = action.payload;
+      const itemIdToRemove = action.payload.card.info.id;
+      //Get the id of item to substract price and then filter it when id of both matches and then substract from totalPrice
+      const priceIdToRemove =
+        action.payload.card.info.price || action.payload.card.info.defaultPrice; // Get item price
+      //Removing items from cart
       state.items = state.items.filter((item) => {
         return item.card.info.id !== itemIdToRemove;
       });
+      state.totalPrice -= priceIdToRemove;
     },
     clearCart: (state, action) => {
       state.items.length = 0; // => [ ]
+      state.totalPrice = 0;
     },
   },
 });
