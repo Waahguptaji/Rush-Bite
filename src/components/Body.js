cc43b2c1b9832a76ab09e08cb025363000b8f928
Sahil Gupta <sahilgupta742@gmail.com>
2024-01-14 10:00:29 +0530

    Body Responsive And add filter Options

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,10 +9,12 @@ import { CDN_URL } from "../utils/constants";
 const Body = () => {
   //Local State Variable - Super powerful Variable
   const [listofRestuarants, setlistofRestuarants] = useState([]);
+  const [listofTopRestuarants, setlistofTopRestuarants] = useState([]);
   const [filterRestuarants, setfilterRestuarants] = useState([]);
 
   const [searchText, setsearchText] = useState("");
   const [foodItem, setFoodItem] = useState([]);
+  const [filters, setFilters] = useState([]);
   const onlineStatus = useOnlineStatus();
 
   const [hasMore, setHasMore] = useState(true);
@@ -32,19 +34,26 @@ const Body = () => {
     const foodItemsInfo =
       json?.data?.cards[0]?.card?.card?.imageGridCards?.info;
 
-    console.log(json?.data);
+    const topResturantData =
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+
+    const filterOption = json?.data?.cards[3]?.card?.card?.facetList;
+
     const restaurantsData =
       json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants ||
       json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants;
 
-    setFoodItem(foodItemsInfo);
     if (!restaurantsData || restaurantsData.length === 0) {
       setHasMore(false); // No more data available
       return;
     }
 
+    setFilters(filterOption);
+    setFoodItem(foodItemsInfo);
+    setlistofTopRestuarants(topResturantData);
     setlistofRestuarants([...listofRestuarants, ...restaurantsData]); //it is taking each new array restuarant data on each fetch and combining it into new one array listofResturant
     setfilterRestuarants([...filterRestuarants, ...restaurantsData]);
     setPage(page + 1); // Increment page number
@@ -68,16 +77,14 @@ const Body = () => {
       <div className=" sm:w-9/12 sm:mx-auto ">
         <div className="p-4">
           <div className="mb-4">
-            <h2 className="font-extrabold text-2xl ">
-              Sahil, What's on your mind?
-            </h2>
+            <h2 className="font-bold text-2xl ">Sahil, What's on your mind?</h2>
           </div>
-          <div className=" sm:mx-[-16px] overflow-x-scroll overflow-y-hidden no-scrollbar">
-            <div className=" grid row-span-2 sm:flex sm:pt-0 ">
+          <div className=" mx-[-16px] overflow-x-scroll overflow-y-hidden no-scrollbar">
+            <div className=" grid grid-flow-col sm:flex sm:pt-0 ">
               {foodItem.map((item, index) => (
                 <div className="pr-6 first:pl-4 w-full">
                   <img
-                    className=" w-28 sm:w-[150px] max-w-xl"
+                    className=" w-20 sm:w-[146px] max-w-xl"
                     key={item.id}
                     src={CDN_URL + item.imageId}
                   ></img>
@@ -124,18 +131,17 @@ const Body = () => {
           </button>
         </div>
       </div> */}
-      <hr className="sm:w-9/12 sm:mx-auto" />
-      {/* InfiniteScroll using react-component-infinite-scroll */}
-      <InfiniteScroll
-        dataLength={filterRestuarants.length}
-        next={fetchMoreData}
-        hasMore={hasMore}
-        loader={<Shimmer />} // Loader component while fetching data
-        endMessage={<p>No more restaurants to show.</p>} // Message when no more data available
-      >
-        <div className=" sm:w-9/12 sm:mx-auto ">
-          <div className="grid grid-cols-4 items-start gap-4 my-4 mx-2 ease-in-out">
-            {filterRestuarants.map(
+      <hr className="sm:w-9/12 sm:mx-auto mb-8  " />
+
+      <div className=" w-full sm:w-9/12 sm:mx-auto ">
+        <div className="px-4">
+          <div className="mb-4">
+            <h2 className="font-bold text-2xl ">
+              Top restaurant chains in Indore
+            </h2>
+          </div>
+          <div className=" sm:grid sm:grid-cols-4 items-start gap-4 my-4 mx-2 overflow-x-scroll overflow-y-hidden no-scrollbar ">
+            {listofTopRestuarants.map(
               (
                 restaurant //Map function to iterate over the data dynamically
               ) => (
@@ -147,6 +153,48 @@ const Body = () => {
                 </Link>
               )
             )}
+          </div>
+        </div>
+      </div>
+      <hr className="sm:w-9/12 sm:mx-auto mt-8 mb-8 " />
+      {/* InfiniteScroll using react-component-infinite-scroll */}
+      <div className=" sm:w-9/12 sm:mx-auto ">
+        <div className=" p-4 ">
+          <div className="font-bold text-2xl ">
+            Restaurants with online food delivery in Indore.{" "}
+          </div>
+          <div className="flex my-4 mt-5 gap-2 overflow-y-scroll no-scrollbar cursor-pointer">
+            {filters.map((filter) => (
+              <div className="flex text-sm border rounded-3xl px-2 p-[6px]  whitespace-nowrap ">
+                {filter.label}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <InfiniteScroll
+        dataLength={filterRestuarants.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<Shimmer />} // Loader component while fetching data
+        endMessage={<p>No more restaurants to show.</p>} // Message when no more data available
+      >
+        <div className=" w-full sm:w-9/12 sm:mx-auto ">
+          <div className="px-4">
+            <div className="flex flex-col sm:grid sm:grid-cols-4 items-start gap-4 my-4 mx-2 ">
+              {filterRestuarants.map(
+                (
+                  restaurant //Map function to iterate over the data dynamically
+                ) => (
+                  <Link
+                    key={restaurant.info.id}
+                    to={"/restuarants/" + restaurant.info.id}
+                  >
+                    <RestuarantCard resData={restaurant} />
+                  </Link>
+                )
+              )}
+            </div>
           </div>
         </div>
       </InfiniteScroll>
--- src/components/RestuarantCard.js
+++ src/components/RestuarantCard.js
@@ -4,20 +4,20 @@ const RestuarantCard = (props) => {
   const { resData } = props;
   const { name, cuisines, avgRating, costForTwo, sla } = resData?.info; //optional chaining
   return (
-    <div className="grid gap-3 cursor-pointer items-center justify-stretch grid-flow-row ">
-      <div className=" w-full h-full relative overflow-hidden filter">
+    <div className="grid grid-flow-col gap-4 sm:grid sm:gap-3 cursor-pointer items-center sm:justify-stretch sm:grid-flow-row ">
+      <div className=" w-full h-full relative overflow-hidden drop-shadow-lg">
         <img
-          className=" w-60 h-40 object-cover rounded-2xl "
+          className=" w-36 h-44  sm:w-60 sm:h-40 object-cover rounded-2xl "
           src={CDN_URL + resData.info.cloudinaryImageId}
           alt="dishImg"
         />
       </div>
-      <div className="ml-3 overflow-y-auto">
+      <div className="overflow-x-auto sm:ml-3 sm:overflow-y-auto">
         <h1 className=" text-lg font-bold text-gray-900 overflow-hidden w-full text-ellipsis whitespace-nowrap  break-words">
           {name}
         </h1>
         <h2 className="grid grid-flow-col items-center mt-[2px] gap-[2px] justify-start">
-          <p>
+          <p className="text-green-600">
             <FaStar />
           </p>
           <p className="text-lg font-bold text-gray-900 overflow-hidden w-full break-words">
